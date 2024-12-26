c6b624f9cbc19060f42c9b451ab7107e198a0c81
Feras Jobeir <fjobeir@gmail.com>
2024-12-26 10:54:22 +0300

    install gtm

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -10,6 +10,7 @@ import { SessionProvider } from "next-auth/react";
 import { auth } from "../../../auth";
 import Header from "@/components/shared/header";
 import Footer from "@/components/shared/footer";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const ibm = IBM_Plex_Sans_Arabic({
   subsets: ["arabic"],
@@ -37,6 +38,7 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
+      <GoogleTagManager gtmId="G-ECLQC58Q2W" />
       <body className={`${ibm.className} antialiased`}>
       <SessionProvider session={session}>
         <NextIntlClientProvider messages={messages}>
