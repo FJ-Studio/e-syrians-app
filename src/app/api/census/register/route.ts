60332cbc1424bcc71a10c02ef0a0e4624cebe0e1
Feras Jobeir <fjobeir@gmail.com>
2025-02-25 20:19:59 +0300

    Comment out recaptcha validation logic in census registration route

--- src/app/api/census/register/route.ts
+++ src/app/api/census/register/route.ts
@@ -1,19 +1,19 @@
 // import recaptchaIsValid from "@/lib/recaptcha";
-import recaptchaIsValid from "@/lib/recaptcha";
+// import recaptchaIsValid from "@/lib/recaptcha";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.formData();
-    const isHuman = await recaptchaIsValid(
-      body.get("recaptcha_token") as string
-    );
-    if (!isHuman) {
-      return NextResponse.json(
-        { messages: ["invalid_recaptcha_token"], success: false },
-        { status: 400 }
-      );
-    }
+    // const isHuman = await recaptchaIsValid(
+    //   body.get("recaptcha_token") as string
+    // );
+    // if (!isHuman) {
+    //   return NextResponse.json(
+    //     { messages: ["invalid_recaptcha_token"], success: false },
+    //     { status: 400 }
+    //   );
+    // }
     const request = await fetch(`${process.env.API_URL}/users/register`, {
       method: "POST",
       headers: {
