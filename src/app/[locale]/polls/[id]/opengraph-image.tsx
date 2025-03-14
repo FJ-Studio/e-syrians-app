2d3340a1da33d9aab3051c706fa4deea305d7979
Feras Jobeir <fjobeir@gmail.com>
2025-03-14 03:46:32 +0300

    Enhance OpenGraph image generation with improved styling and dynamic option display

--- src/app/[locale]/polls/[id]/opengraph-image.tsx
+++ src/app/[locale]/polls/[id]/opengraph-image.tsx
@@ -1,42 +1,67 @@
-import { getPoll } from '@/lib/api/requests';
-import { Locale } from '@/lib/types/locale';
-import { ImageResponse } from 'next/og'
-// import { readFile } from 'node:fs/promises'
-// import { join } from 'node:path'
+import { getPoll } from "@/lib/api/requests";
+import { Locale } from "@/lib/types/locale";
+import { ImageResponse } from "next/og";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 
 type Props = {
   params: Promise<{ locale: Locale; id: string }>;
 };
- 
+
 // Image metadata
 export const size = {
   width: 1200,
   height: 630,
-}
- 
-export const contentType = 'image/png'
- 
+};
+
+export const contentType = "image/png";
+
 // Image generation
 export default async function Image({ params }: Props) {
   // Font loading, process.cwd() is Next.js project directory
   const { id } = await params;
-    const poll = await getPoll(id);
- 
+  const poll = await getPoll(id);
+  const font = await readFile(
+    join(process.cwd(), "src/lib/fonts/IBMPlexSansArabic-SemiBold.ttf")
+  );
+  //
+
   return new ImageResponse(
     (
       // ImageResponse JSX element
       <div
         style={{
-          fontSize: 128,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          fontSize: 30,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+          fontFamily: "IBMPlexSansArabic-SemiBold",
+          backgroundColor: '#DFDBE5',
+          backgroundImage: 'linear-gradient(315deg, #DFDBE5 0%, #E7E4EE 74%)',
+            color: '#333',
         }}
       >
-        {poll.data.question}
+        <div style={{ padding: 20, display: "flex", flexDirection: "column" }}>
+          <p>{poll.data.question.substring(0, 100)}</p>
+          {poll.data.options.slice(0, 5).map((option, index) => (
+            <div
+              key={index}
+              style={{
+                border: "1px solid #999",
+                borderRadius: "4px",
+                marginBottom: "10px",
+                padding: "2px 6px",
+                color: "#333",
+              }}
+            >
+              {option.option_text}
+            </div>
+          ))}
+        </div>
       </div>
     ),
     // ImageResponse options
@@ -44,6 +69,14 @@ export default async function Image({ params }: Props) {
       // For convenience, we can re-use the exported opengraph-image
       // size config to also set the ImageResponse's width and height.
       ...size,
+      fonts: [
+        {
+          name: "IBMPlexSansArabic-SemiBold",
+          data: font,
+          style: "normal",
+          weight: 600,
+        },
+      ],
     }
-  )
+  );
 }
