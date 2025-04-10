4744e25ac1d8b41dad354967bb3ba376bc71d670
Feras Jobeir <fjobeir@gmail.com>
2025-04-10 09:24:23 +0300

    Update sitemap URLs to include 'www' for consistency

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,46 +3,46 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://e-syrians.com",
+      url: "https://www.e-syrians.com",
       lastModified: new Date(),
       alternates: {
         languages: {
-          ar: "https://e-syrians.com/ar",
-          en: "https://e-syrians.com/en",
-          ku: "https://e-syrians.com/ku",
+          ar: "https://www.e-syrians.com/ar",
+          en: "https://www.e-syrians.com/en",
+          ku: "https://www.e-syrians.com/ku",
         },
       },
     },
     {
-      url: "https://e-syrians.com/faq",
+      url: "https://www.e-syrians.com/faq",
       lastModified: new Date(),
       alternates: {
         languages: {
-          ar: "https://e-syrians.com/ar/faq",
-          en: "https://e-syrians.com/en/faq",
-          ku: "https://e-syrians.com/ku/faq",
+          ar: "https://www.e-syrians.com/ar/faq",
+          en: "https://www.e-syrians.com/en/faq",
+          ku: "https://www.e-syrians.com/ku/faq",
         },
       },
     },
     {
-      url: "https://e-syrians.com/census",
+      url: "https://www.e-syrians.com/census",
       lastModified: new Date(),
       alternates: {
         languages: {
-          ar: "https://e-syrians.com/ar/census",
-          en: "https://e-syrians.com/en/census",
-          ku: "https://e-syrians.com/ku/census",
+          ar: "https://www.e-syrians.com/ar/census",
+          en: "https://www.e-syrians.com/en/census",
+          ku: "https://www.e-syrians.com/ku/census",
         },
       },
     },
     {
-      url: "https://e-syrians.com/polls",
+      url: "https://www.e-syrians.com/polls",
       lastModified: new Date(),
       alternates: {
         languages: {
-          ar: "https://e-syrians.com/ar/polls",
-          en: "https://e-syrians.com/en/polls",
-          ku: "https://e-syrians.com/ku/polls",
+          ar: "https://www.e-syrians.com/ar/polls",
+          en: "https://www.e-syrians.com/en/polls",
+          ku: "https://www.e-syrians.com/ku/polls",
         },
       },
     },
