cff6b4691eddea38be4e4d33b89512e7b1b7581f
Feras Jobeir <fjobeir@users.noreply.github.com>
2026-03-27 15:53:20 +0300

    Create opengraph-image.tsx

---
+++ src/app/[locale]/polls/[id]/opengraph-image.tsx
@@ -0,0 +1,61 @@
+import { getPoll } from '@/lib/api/requests';
+import { Locale } from '@/lib/types/locale';
+import { ImageResponse } from 'next/og'
+import { readFile } from 'node:fs/promises'
+import { join } from 'node:path'
+
+type Props = {
+  params: Promise<{ locale: Locale; id: string }>;
+};
+ 
+// Image metadata
+export const alt = 'About Acme'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+ 
+export const contentType = 'image/png'
+ 
+// Image generation
+export default async function Image({ params }: Props) {
+  // Font loading, process.cwd() is Next.js project directory
+  const { id } = await params;
+    const poll = await getPoll(id);
+  const interSemiBold = await readFile(
+    join(process.cwd(), 'assets/Inter-SemiBold.ttf')
+  )
+ 
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {poll.data.question}
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: 'Inter',
+          data: interSemiBold,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
+    }
+  )
+}
