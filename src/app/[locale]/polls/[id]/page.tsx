469e1f71c0094a308af11f9a2b186cf80ffde0ef
Feras Jobeir <fjobeir@users.noreply.github.com>
2026-03-27 15:57:45 +0300

    Update page.tsx

--- src/app/[locale]/polls/[id]/page.tsx
+++ src/app/[locale]/polls/[id]/page.tsx
@@ -6,24 +6,24 @@ type Props = {
   params: Promise<{ locale: Locale; id: string }>;
 };
 
-export async function generateMetadata({ params }: Props) {
-  const { id } = await params;
-  const poll = await getPoll(id);
-  return {
-    title: poll.data.question,
-    openGraph: {
-      title: poll.data.question,
-      images: [
-        {
-          url: "https://www.e-syrians.com/e-syrians-polls-og.png",
-          width: 1200,
-          height: 630,
-          alt: "E-SYRIANS Network",
-        },
-      ],
-    }
-  }
-}
+// export async function generateMetadata({ params }: Props) {
+//   const { id } = await params;
+//   const poll = await getPoll(id);
+//   return {
+//     title: poll.data.question,
+//     openGraph: {
+//       title: poll.data.question,
+//       images: [
+//         {
+//           url: "https://www.e-syrians.com/e-syrians-polls-og.png",
+//           width: 1200,
+//           height: 630,
+//           alt: "E-SYRIANS Network",
+//         },
+//       ],
+//     }
+//   }
+// }
 
 
 export default async function SinglePollsPage({ params }: Props) {
