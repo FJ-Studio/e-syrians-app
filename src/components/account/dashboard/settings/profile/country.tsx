97ee90dcb82422bfafc2069e18a26b6ade6b96de
Feras <fjobeir@gmail.com>
2026-04-23 02:01:48 +0300

    feat: add image cropping functionality for avatar uploads

--- src/components/account/dashboard/settings/profile/country.tsx
+++ src/components/account/dashboard/settings/profile/country.tsx
@@ -5,7 +5,7 @@ import extractErrors from "@/lib/extract-errors";
 import { generateToken } from "@/lib/recaptcha";
 import { ESUser } from "@/lib/types/account";
 import { CountryCode } from "@/lib/types/misc";
-import { Autocomplete, AutocompleteItem, Avatar, Button, Card, CardBody, CardHeader, Skeleton } from "@heroui/react";
+import { Autocomplete, AutocompleteItem, Avatar, Button, Card, CardBody, CardHeader } from "@heroui/react";
 import { useTranslations } from "next-intl";
 import { FC, useEffect } from "react";
 import { Controller, useForm, useWatch } from "react-hook-form";
@@ -75,69 +75,65 @@ const AccountAddress: FC<UpdateAddressProps> = ({ user }) => {
       </CardHeader>
       <CardBody>
         <form onSubmit={handleSubmit(save)} className="space-y-4">
-          <Skeleton isLoaded={!!user} className="rounded-lg">
+          <Controller
+            name="country"
+            control={control}
+            rules={{ required: true }}
+            render={({ field, fieldState: { error, invalid } }) => (
+              <Autocomplete
+                {...field}
+                label={t("fields.country.label")}
+                isRequired
+                classNames={{
+                  clearButton: "hidden",
+                }}
+                scrollShadowProps={{
+                  isEnabled: false,
+                }}
+                isInvalid={invalid}
+                errorMessage={error?.message}
+                selectedKey={getValues("country")}
+                onSelectionChange={(selected) => {
+                  setValue("country", selected?.toString() as CountryCode);
+                }}
+                description={t("fields.country.description")}
+              >
+                {Object.keys(countries).map((country) => (
+                  <AutocompleteItem
+                    key={country}
+                    startContent={<Avatar src={`/flags/${country.toLowerCase()}.svg`} className="h-6 w-6" size="sm" />}
+                  >
+                    {countries[country as keyof typeof countries]}
+                  </AutocompleteItem>
+                ))}
+              </Autocomplete>
+            )}
+          />
+          {countryValue === "SY" && (
             <Controller
-              name="country"
+              name="city_inside_syria"
               control={control}
               rules={{ required: true }}
               render={({ field, fieldState: { error, invalid } }) => (
                 <Autocomplete
                   {...field}
-                  label={t("fields.country.label")}
+                  label={t("fields.city_inside_syria.label")}
                   isRequired
-                  classNames={{
-                    clearButton: "hidden",
-                  }}
-                  scrollShadowProps={{
-                    isEnabled: false,
-                  }}
                   isInvalid={invalid}
                   errorMessage={error?.message}
-                  selectedKey={getValues("country")}
+                  selectedKey={getValues("city_inside_syria")}
                   onSelectionChange={(selected) => {
-                    setValue("country", selected?.toString() as CountryCode);
+                    setValue("city_inside_syria", selected?.toString() ?? "");
                   }}
-                  description={t("fields.country.description")}
+                  classNames={{ clearButton: "hidden" }}
                 >
-                  {Object.keys(countries).map((country) => (
-                    <AutocompleteItem
-                      key={country}
-                      startContent={
-                        <Avatar src={`/flags/${country.toLowerCase()}.svg`} className="h-6 w-6" size="sm" />
-                      }
-                    >
-                      {countries[country as keyof typeof countries]}
-                    </AutocompleteItem>
+                  {Object.keys(provinces).map((key) => (
+                    <AutocompleteItem key={key}>{provinces[key as keyof typeof provinces]}</AutocompleteItem>
                   ))}
                 </Autocomplete>
               )}
             />
-            {countryValue === "SY" && (
-              <Controller
-                name="city_inside_syria"
-                control={control}
-                rules={{ required: true }}
-                render={({ field, fieldState: { error, invalid } }) => (
-                  <Autocomplete
-                    {...field}
-                    label={t("fields.city_inside_syria.label")}
-                    isRequired
-                    isInvalid={invalid}
-                    errorMessage={error?.message}
-                    selectedKey={getValues("city_inside_syria")}
-                    onSelectionChange={(selected) => {
-                      setValue("city_inside_syria", selected?.toString() ?? "");
-                    }}
-                    classNames={{ clearButton: "hidden" }}
-                  >
-                    {Object.keys(provinces).map((key) => (
-                      <AutocompleteItem key={key}>{provinces[key as keyof typeof provinces]}</AutocompleteItem>
-                    ))}
-                  </Autocomplete>
-                )}
-              />
-            )}
-          </Skeleton>
+          )}
           <Button color="primary" type="submit" isLoading={isSubmitting} isDisabled={!isDirty || isSubmitting}>
             {t("save")}
           </Button>
