f8aac3814e929c99d2a8852524d8ff713f1c63fd
Feras <fjobeir@gmail.com>
2026-04-26 15:43:54 +0300

    feat: Wrap Donate component in PageLayout for improved structure

--- src/app/[locale]/donate/page.tsx
+++ src/app/[locale]/donate/page.tsx
@@ -1,5 +1,10 @@
 import Donate from "@/components/account/dashboard/donate/donate";
+import PageLayout from "@/components/pages/layout";
 
 export default function DonatePage() {
-  return <Donate />;
+  return (
+    <PageLayout>
+      <Donate />
+    </PageLayout>
+  );
 }
